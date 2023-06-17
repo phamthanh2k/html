f1e864b0dc1781e3d04a1ced312bc26be11fa422
phamthanh2k <phamtinh2n@gmail.com>
2023-06-17 18:21:10 +0700

    d

---
+++ javascript/lesson_1/main.js
@@ -0,0 +1,30 @@
+// var fullName = 'dang ngoc son'
+// var age = '19'
+// alert(fullName)
+// alert(age)
+
+// console.error('sadsadsad')
+var a = 1;
+var b = 2;
+var c = 3;
+
+if ((a > 0 && b < 0) || c > 0) {
+  console.log("dieu kien dung ");
+} else {
+  console.log("dieu kien sai");
+}
+
+var myFunction = function () {
+  alert("day la 1 function");
+};
+
+var myObject = {
+  name: "value",
+  age: 18,
+  adress: "hanoi",
+};
+console.log("object", myObject);
+
+var myArray = ["java", "php", "html", "css"];
+
+console.log(myArray)
